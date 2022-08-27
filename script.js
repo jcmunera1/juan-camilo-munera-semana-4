8e27f43dc4bb660d63d6513b496a2ab894acbdbf
Juan Camilo Munera <jcmunera1@gmail.com>
2022-08-27 15:30:04 -0500

    Addition of line-through event

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ let data = [
   "Ir al Gimnasio",
   "Leer twilight",
 ];
+
+// Getting an element from the html
 let list = document.getElementById("myList");
 data.forEach((item) => {
+  // Create Element
   let li = document.createElement("li");
   li.textContent = item;
+
+  // Add Element
   list.appendChild(li);
-  li.addEventListener("click", () => completarItem());
+
+  // Event Listener: listen to event "click", when this happens, call the function "completarItem".
+  li.addEventListener("click", (event) => completarItem(event));
 });
+
+// Function called when the event "clicked" its active.
+function completarItem(event) {
+  console.log(event.target);
+
+  // Do a forEach loop to iterate over the items in the list and change the style...
+  list.style.textDecorationLine = "line-through	";
+}
